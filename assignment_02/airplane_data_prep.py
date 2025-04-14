6fd909e81026b6578dbb4080400a22ac74429abf
BRC1004 <br723375@ucf.edu>
2025-04-13 21:45:08 -0400

    Update airplane_data_prep.py

--- assignment_02/airplane_data_prep.py
+++ assignment_02/airplane_data_prep.py
@@ -20,7 +20,11 @@
 # Import Required Modules
 ##################################################
 
+import pandas as pd
+import os
 
+# Regression modeling module
+import statsmodels.formula.api as sm
 
 ##################################################
 # Set up Workspace
@@ -39,7 +43,7 @@ print(os.path.dirname(os.path.realpath(__file__)))
 # Change to a new directory.
 
 # You could set it directly from the location of this file
-os.chdir(os.path.dirname(os.path.realpath(__file__)))
+os.chdir('C:/Users/BRC10/OneDrive/Documents/GitHub/Brian-Carter-QMB6315-Solutions/assignment_02')
 
 # Check that the change was successful.
 os.getcwd()
@@ -52,51 +56,94 @@ os.getcwd()
 ##################################################
 
 
+airplane_xlsx = pd.ExcelFile('airplane_data.xlsx')
 
+
+airplane_sales = pd.read_excel(airplane_xlsx, 'airplane_sales')
+
+
+airplane_sales.describe()
 
 #--------------------------------------------------
 # Fit a regression model.
 #--------------------------------------------------
 
+reg_model_sales = sm.ols(formula = "price ~ age", data = airplane_sales).fit()
 
+
+
+print(reg_model_sales.summary())
 
 
 ##################################################
 # Part b) Read Specification Data
 ##################################################
 
+airplane_xlsx = pd.ExcelFile('airplane_data.xlsx')
 
+airplane_specs = pd.read_excel(airplane_xlsx,  'airplane_specs')
+
+airplane_specs.describe()
 
 #--------------------------------------------------
 # Join the two datasets together.
 #--------------------------------------------------
 
+airplane_sales_specs = pd.concat([airplane_sales, airplane_specs], axis = 1)
 
+airplane_sales_specs.describe()
+
+airplane_sales_specs.columns
 
 
 #--------------------------------------------------
 # Fit another regression model.
 #--------------------------------------------------
 
+reg_model_sales_specs = sm.ols(formula = 
+                           "price ~ age + passengers + wtop + fixgear + tdrag", 
+                           data = airplane_sales_specs).fit()
 
+
+print(reg_model_sales_specs.summary())
 
 
 ##################################################
 # Part c) Read Performance Data
 ##################################################
 
+airplane_xlsx = pd.ExcelFile('airplane_data.xlsx')
 
+airplane_perf = pd.read_excel(airplane_xlsx,  'airplane_perf')
+
+airplane_perf.describe()
 
 #--------------------------------------------------
 # Join the third dataset to the first two.
 #--------------------------------------------------
 
 
+airplane_full = pd.concat([airplane_sales_specs, airplane_perf], axis = 1)
+
+
+airplane_full.describe()
+
+airplane_full.columns
 
 
 #--------------------------------------------------
 # Fit another regression model.
 #--------------------------------------------------
+
+reg_model_full = sm.ols(formula = 
+                           "price ~ age + passengers + wtop \
+                               + fixgear + tdrag + horse \
+                                   + fuel + ceiling + cruise", 
+                           data = airplane_full).fit()
+
+
+# Display a summary table of regression results.
+print(reg_model_full.summary())
 
 
 
